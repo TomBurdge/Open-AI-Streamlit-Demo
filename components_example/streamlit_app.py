b60397a074492f5937639f7422f792d18113ebbe
Tom Burdge <tomburdge3@gmail.com>
2023-11-21 12:14:56 +0000

    init - remove irrelevant files

--- components_example/streamlit_app.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-
-st.title("Streamlit Components Examples")
-st.write(
-    """This app contains examples of
-    Streamlit components, find them
-    all in the sidebar!"""
-)
--- mpa_example/streamlit_app.py
+++
@@ -1,4 +0,0 @@
-import streamlit as st
-
-st.title("MPA Example")
-st.write("Here is our first multipage app!")
