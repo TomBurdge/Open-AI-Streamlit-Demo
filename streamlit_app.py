ccaae598550c7e5942524b7fb989a234e686f878
Tom Burdge <tomburdge3@gmail.com>
2023-11-21 15:16:44 +0000

    fix - add humility to prompt

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,7 +17,7 @@ client = OpenAI(
 
 system_message = """
 You are a helpful assistant for finding out the parent of an FMCG brand/manufacturer.
-You always respond with the parent company of a brand/manufacturer that you receive. You also return a confidence score between 0 and 1. You only return these two things.
+You always respond with the parent company of a brand/manufacturer that you receive. If you do not know, return 'Unknown' instead of a boolean. You also return a confidence score between 0 and 1. You only return these two things.
 For example, the parent of Johnson's Baby is Kenvue. Another example, the parent brand of MountainDew is PepsiCo.
 You may receive further information, such as the geographic location of this brand, but not always.
 """
